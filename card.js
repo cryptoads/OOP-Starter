5ee6fd67d6461f907221d1d8ae101061da6f1566
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-24 22:18:55 -0400

    finished the constructors

--- card.js
+++ card.js
@@ -21,10 +21,43 @@ Hand.prototype.getPoints = function(){
     return this.points;
 };
 
+function Deck(){
+    this.deck = [];
+    var suits = ['diamonds', 'hearts', 'clubs', 'spades'];
+    for (var i = 0; i < 52; i++) {
+        var newCardsSuit = suits[Math.floor(i / 13)];
+        var newCardsPoint = (i % 13) + 1;
+        var newCard = new Card(newCardsPoint, newCardsSuit);
+        this.deck.push(newCard);
+    };
+};
+
+Deck.prototype.draw = function(){
+    var card = this.deck.pop();
+    return card;
+};
+
+Deck.prototype.shuffle = function(){
+    var shuffledDeck = this.deck.sort(function(a,b){return .5 - Math.random()});
+    return shuffledDeck;
+};
+
+Deck.prototype.numCardsLeft = function(){
+    return this.deck.length;
+};
+
+
 var myCard = new Card(5, 'diamonds');
 var myHand = new Hand();
+console.log(myCard.getImageUrl(myCard));
 myHand.addCard(new Card(5, 'diamonds'));
 myHand.addCard(new Card(13, 'spades'));
 console.log(myHand.hand);
 console.log(myHand.getPoints());
+var myDeck = new Deck();
+myDeck.shuffle();
+console.log(myDeck.deck);
+console.log(myDeck.numCardsLeft());
+console.log(myDeck.draw());
+console.log(myDeck.numCardsLeft());
 console.log(myCard.getImageUrl(myCard));
